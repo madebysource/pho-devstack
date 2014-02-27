3608d35ef67bff5b71cda098d9d1238909ab0be9
Jan Sedivy <jansedivy@abdoc.net>
2014-02-27 15:27:23 +0100

    use pat.join in path concatenation

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var plumber = require('gulp-plumber');
 var refresh = require('gulp-livereload');
 var rev = require('gulp-rev');
 var jasmine = require('gulp-jasmine');
+var path = require('path');
 
 var extend = require('node.extend');
 var defaultConfig = require('./config');
@@ -22,15 +23,15 @@ module.exports = function(gulp, userConfig) {
 
   gulp.task('dist-clean', function(cb) {
     gulp.src([
-      config.dist.scriptDir + config.dist.scriptFiles,
-      config.dist.styleDir + config.dist.styleFiles
+      path.join(config.dist.scriptDir, config.dist.scriptFiles),
+      path.join(config.dist.styleDir, config.dist.styleFiles)
     ], { read: false })
       .pipe(clean())
       .on('end', cb);
   });
 
   gulp.task('scripts', ['dist-clean'], function(cb) {
-    gulp.src(config.src.scriptDir + config.src.scriptMain)
+    gulp.src(path.join(config.src.scriptDir, config.src.scriptMain))
       .pipe(plumber(config.plumber))
       .pipe(browserify(config.browserify))
       .pipe(rev())
@@ -39,7 +40,7 @@ module.exports = function(gulp, userConfig) {
   });
 
   gulp.task('styles', ['dist-clean'], function(cb) {
-    gulp.src(config.src.styleDir + config.src.styleMain)
+    gulp.src(path.join(config.src.styleDir, config.src.styleMain))
       .pipe(plumber(config.plumber))
       .pipe(less(config.less))
       .pipe(rev())
@@ -49,11 +50,11 @@ module.exports = function(gulp, userConfig) {
 
   gulp.task('index', ['scripts', 'styles'], function(cb) {
     gulp.src([
-      config.dist.scriptDir + config.dist.scriptFiles,
-      config.dist.styleDir + config.dist.styleFiles
+      path.join(config.dist.scriptDir, config.dist.scriptFiles),
+      path.join(config.dist.styleDir, config.dist.styleFiles)
     ], { read: false })
       .pipe(plumber(config.plumber))
-      .pipe(inject(config.src.markupDir + config.src.markupMain, config.inject))
+      .pipe(inject(path.join(config.src.markupDir, config.src.markupMain), config.inject))
       .pipe(htmlmin(config.htmlmin))
       .pipe(gulp.dest('dist'))
       .pipe(refresh(lrServer))
@@ -62,13 +63,13 @@ module.exports = function(gulp, userConfig) {
 
   gulp.task('default', ['index', 'lrServer'], function() {
     gulp.watch([
-      config.src.markupDir + config.src.markupFiles,
-      config.src.scriptDir + config.src.scriptFiles,
-      config.src.styleDir + config.src.styleFiles
+      path.join(config.src.markupDir, config.src.markupFiles),
+      path.join(config.src.scriptDir, config.src.scriptFiles),
+      path.join(config.src.styleDir, config.src.styleFiles)
     ], ['index']);
   });
 
   gulp.task('test', function() {
-    gulp.src(config.src.specDir + config.src.specFiles).pipe(jasmine());
+    gulp.src(path.join(config.src.specDir, config.src.specFiles)).pipe(jasmine());
   });
 };
