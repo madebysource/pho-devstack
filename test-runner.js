6b9c21d7f4a0dafabca47f4c9f05509546066604
Jan Sedivy <jansedivy@abdoc.net>
2014-05-15 12:41:41 +0200

    remove gutil dependency

--- test-runner.js
+++ test-runner.js
@@ -1,7 +1,7 @@
 var karma = require('karma');
 var spawn = require('child_process').spawn;
 var path = require('path');
-var gutil = require('gulp-util');
+var chalk = require('chalk');
 
 module.exports = {
   karma: function() {
@@ -16,7 +16,7 @@ module.exports = {
     var casper = spawn(path.join(__dirname, 'node_modules/casperjs/bin/casperjs'), ['test', testPath]);
 
     casper.stdout.on('data', function(data) {
-      gutil.log('CasperJS: ' + data.toString().slice(0, -1));
+      console.log('[' + chalk.green('gulp') + '] CasperJS ' + data.toString().slice(0, -1));
     });
 
     casper.stdout.on('close', process.exit);
